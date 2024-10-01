42a87de981ea03eaaf4579b842bd717666a67dd3
MOsinskyi <105541557+MOsinskyi@users.noreply.github.com>
2024-10-01 15:46:52 +0300

    Add project files.

---
+++ LoveLink/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LoveLink
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LoveLink/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Plugin.Maui.SwipeCardView.Core;
+using LoveLink.ViewModels;
+
+namespace LoveLink;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = new MainPageModel();
+        SwipeCardView.Dragging += OnDragging;
+    }
+
+    private void OnDislikeClicked(object sender, EventArgs e)
+    {
+        SwipeCardView.InvokeSwipe(SwipeCardDirection.Left);
+    }
+
+    private void OnLikeClicked(object sender, EventArgs e)
+    {
+        SwipeCardView.InvokeSwipe(SwipeCardDirection.Right);
+    }
+
+    private void OnDragging(object? sender, DraggingCardEventArgs e)
+    {
+        var view = (View)sender!;
+        var nopeFrame = view.FindByName<Frame>("NopeFrame");
+        var likeFrame = view.FindByName<Frame>("LikeFrame");
+        var threshold = (BindingContext as MainPageModel)!.Threshold;
+
+        var draggedXPercent = e.DistanceDraggedX / threshold;
+
+        switch (e.Position)
+        {
+            case DraggingCardPosition.Start:
+                nopeFrame.Opacity = 0;
+                likeFrame.Opacity = 0;
+                NopeButton.Scale = 1;
+                LikeButton.Scale = 1;
+                break;
+            case DraggingCardPosition.UnderThreshold:
+                if (e.Direction == SwipeCardDirection.Left)
+                {
+                    nopeFrame.Opacity = (-1) * draggedXPercent;
+                    NopeButton.Scale = 1 + draggedXPercent / 2;
+                }
+                else if (e.Direction == SwipeCardDirection.Right)
+                {
+                    likeFrame.Opacity = draggedXPercent;
+                    LikeButton.Scale = 1 - draggedXPercent / 2;
+                }
+                break;
+            case DraggingCardPosition.OverThreshold:
+                if (e.Direction == SwipeCardDirection.Left)
+                {
+                    nopeFrame.Opacity = 1;
+                }
+                else if (e.Direction == SwipeCardDirection.Right)
+                {
+                    likeFrame.Opacity = 1;
+                }
+                break;
+            case DraggingCardPosition.FinishedUnderThreshold:
+                nopeFrame.Opacity = 0;
+                likeFrame.Opacity = 0;
+                NopeButton.Scale = 1;
+                LikeButton.Scale = 1;
+                break;
+            case DraggingCardPosition.FinishedOverThreshold:
+                nopeFrame.Opacity = 0;
+                likeFrame.Opacity = 0;
+                NopeButton.Scale = 1;
+                LikeButton.Scale = 1;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
